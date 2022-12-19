62de9dfb8b734d649a1148821ed982fedc5c3f1b
Vladyslav Yanchenko <vyanch72@gmail.com>
2022-12-19 23:14:44 +0100

    12.18

--- AH_1218/src/Runner.java
+++ AH_1218/src/Runner.java
@@ -1,33 +1,41 @@
 import java.util.Arrays;
+import java.util.function.Function;
 
 public class Runner {
+    static long[] arr = new long[100];
 
-    static long[] arr;
+    static long COUNT = 0;
 
     public static void main(String[] args) {
-        (new Thread(() -> {
-            try {
-                Thread.sleep(20_000);
-                System.out.println("Over 20 seconds!");
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        })).start();
+        Arrays.fill(arr, 0, arr.length - 1, -1);
 
-        //System.out.println(fib(48));
+        for (int i = 0; i <= 5; i++) {
+            System.out.printf("i=%d fib=%d fibTab=%d fibMemo=%d\n", i, fib(i), fibTab(i), fibMemo(i));
+        }
 
-        int n = 49;
+        System.out.println("Testing fib");
+        test(Runner::fib, 5, 10, 25, 50);
+        System.out.println("Testing fibTab");
+        test(Runner::fibTab, 5, 10, 25, 50);
+        System.out.println("Testing fibMemo");
+        test(Runner::fibMemo, 5, 10, 25, 50);
+    }
 
-        arr = new long[n + 1];
+    public static void test(Function<Integer, Long> testFunction, int ...ns) {
+        for (int n : ns) {
+            COUNT = 0;
 
-        /*for (int i = 0; i < arr.length; i++) {
-            arr[i] = -1;
-        }*/
+            long start = System.currentTimeMillis();
+            long value = testFunction.apply(n);
+            long stop = System.currentTimeMillis();
 
-        System.out.println(fibTab(n));
+            System.out.printf("testFunction(%d) = %d, count=%d, elapsedMs=%d\n", n, value, COUNT, stop-start);
+        }
     }
 
     public static long fib(int n) {
+        COUNT++;
+
         if (n < 2) {
             return 1;
         }
@@ -36,18 +44,26 @@ public class Runner {
     }
 
     public static long fibTab(int n) {
-        long[] arr2 = new long[n];
+        if (n < 2) {
+            return 1;
+        }
+
+        long[] arr2 = new long[n + 1];
         arr2[0] = 1;
         arr2[1] = 1;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
+            COUNT++;
+
             arr2[i] = arr2[i - 1] + arr2[i - 2];
         }
-        return arr2[n - 1];
+
+        return arr2[n];
     }
 
     public static long fibMemo(int n) {
-        //System.out.println(Arrays.toString(arr));
+        COUNT++;
+
         if (arr[n] != -1) {
             return arr[n];
         }
